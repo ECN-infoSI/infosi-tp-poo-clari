05aec9102b92be64e4838fa5867e4be59204300a
Larissa Albuquerque <larissaanogueira24@gmail.com>
2024-09-20 09:45:10 +0200

    Creation of a new project

---
+++ WorldOfECN/src/main/java/com/mycompany/worldofecn/WorldOfECN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.worldofecn;
+
+/**
+ *
+ * @author User
+ */
+public class WorldOfECN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
