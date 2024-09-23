6a20895d45236d5a088650157161e96fd72a75b5
claramattos <mattosclara22@gmail.com>
2024-09-23 15:53:29 +0200

    objet

--- WorldOfECN/src/main/java/com/mycompany/worldofecn/PotionSoin.java
+++ WorldOfECN/src/main/java/com/mycompany/worldofecn/PotionSoin.java
@@ -9,5 +9,42 @@ package com.mycompany.worldofecn;
  * @author Clara
  */
 public class PotionSoin extends Objet {
+    private int force;
+    private Point2D pos;
+    private World monde;
+
+    public Point2D getPos() {
+        return pos;
+    }
+
+    public void setPos(Point2D pos) {
+        this.pos = pos;
+    }
+
+    public void disparaitre() {
+        //this.monde.getMonde()[pos.getX()][pos.getY()] = '0'; //precisa? Ou vai ter o heroi em cima ja?
+        this.pos = null;
+        this.force = 0;
+    }
+    
+    public void PotionSoin (int force) {
+        this.force = force;
+    }
+    
+    public void PotionSoin (PotionSoin potion) {
+        this.force = potion.getForce();
+    }
+    
+    public void PotionSoin () {
+        this.force = 0;
+    }
+    
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
     
 }
